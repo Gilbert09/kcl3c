2d626e365d28a9c9e0b4464f22eb863ee5693e7c
Tom (Server) <root@linux.dysign.co.uk>
2015-01-31 12:48:52 -0500

    Corrected upper cases to lower cases of db migration files

--- HouseHunter/db/migrate/20150130133735_create_addresses.rb
+++ HouseHunter/db/migrate/20150130133735_create_addresses.rb
@@ -1,11 +1,11 @@
 class CreateAddresses < ActiveRecord::Migration
   def change
     create_table :addresses do |t|
-      t.String :addressLine1
-      t.String :addressLine2
-      t.String :townCity
-      t.String :postCode
-      t.String :county
+      t.string :addressLine1
+      t.string :addressLine2
+      t.string :townCity
+      t.string :postCode
+      t.string :county
       t.timestamps null: false
     end
   end
