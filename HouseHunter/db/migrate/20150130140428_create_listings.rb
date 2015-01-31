aaf54f851c6bce4c0ebe4831c41515969090d9f9
Jasper van der Valk <jasper.v.d.valk@gmail.com>
2015-01-31 16:02:50 +0000

    testing added database tables

--- HouseHunter/db/migrate/20150130140428_create_listings.rb
+++ HouseHunter/db/migrate/20150130140428_create_listings.rb
@@ -1,7 +1,12 @@
 class CreateListings < ActiveRecord::Migration
   def change
     create_table :listings do |t|
-
+      t.Integer :listingId
+      t.Integer :propertyId
+      t.Integer :views
+      t.String :visible
+      t.String :dateListed
+      t.String :dateSold
       t.timestamps null: false
     end
   end
