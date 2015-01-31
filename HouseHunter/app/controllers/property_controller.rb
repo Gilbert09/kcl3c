9a59a970d025c9bbf65bbd876b9bee82ec0b7f83
Jasper van der Valk <jasper.v.d.valk@gmail.com>
2015-01-31 17:10:15 +0000

    added controller method to user controller and added the params thing

--- HouseHunter/app/controllers/property_controller.rb
+++ HouseHunter/app/controllers/property_controller.rb
@@ -4,4 +4,9 @@ class PropertyController < ApplicationController
 
   def index
   end
+
+  private
+  def property_params
+    params.require(:property).permit(:price, :numberBedroom, :numberBathroom, :numberRoom, :keyword, :description, :type)
+  end
 end
--- HouseHunter/app/controllers/user_controller.rb
+++ HouseHunter/app/controllers/user_controller.rb
@@ -1,2 +1,17 @@
 class UserController < ApplicationController
+  def show
+  end
+
+  def new
+  end
+
+  def edit
+  end
+
+  #def delete end
+
+  private
+  def user_params
+    params.require(:user).permit(:firstName, :lastName, :password, :title, :email)
+  end
 end
