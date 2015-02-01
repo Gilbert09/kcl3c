747a913f55dbaf40672f411d4409c60de1cc4174
Jasper van der Valk <jasper.v.d.valk@gmail.com>
2015-02-01 16:55:15 +0000

    added a property but it is not seeding to the db

--- HouseHunter/db/seeds.rb
+++ HouseHunter/db/seeds.rb
@@ -6,5 +6,6 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-User.create( firstName: 'Jasper',  lastName: 'van der Valk', password: 'p@ssw0rd123', title: 'Mr', email: 'jasper.v.d.valk@gmail.com')
-Address.create( addressLine1: '65 Henshaw Street', addressLine2: '', townCity: 'London', postCode: 'SE171PE', county: '')
+User.create( firstName: 'Jasper',  lastName: 'van der Valk', password: 'p@ssw0rd123', title: 'Mr', email: 'jasper.v.d.valk@gmail.com' )
+Address.create( addressLine1: '65 Henshaw Street', addressLine2: '', townCity: 'London', postCode: 'SE171PE', county: '' )
+Property.create( price: 500000, numberBedroom: 4, numberBathroom: 2, numberRoom: 8, keyword: 'garage', description: '18th century', type: 'attached' )
