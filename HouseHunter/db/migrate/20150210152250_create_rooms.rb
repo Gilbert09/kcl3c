ddcea9368779bd4eab2d9fc644ded4b66169d212
Jasper van der Valk <jasper.v.d.valk@gmail.com>
2015-02-10 15:48:06 +0000

    changed height to length

--- HouseHunter/db/migrate/20150210152250_create_rooms.rb
+++ HouseHunter/db/migrate/20150210152250_create_rooms.rb
@@ -3,7 +3,7 @@ class CreateRooms < ActiveRecord::Migration
     create_table :rooms do |t|
       t.integer :property_id
       t.integer :multimedia_id
-      t.float :room_height
+      t.float :room_length
       t.float :room_width
       t.string :room_type
       t.string :measurement_unit
