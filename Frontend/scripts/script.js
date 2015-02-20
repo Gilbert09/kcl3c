ea4c4d81e96023183e8add6875c55fda84a0ebb1
Thomas Owers <owerstom@gmail.com>
2015-02-20 18:35:36 +0000

    Header/footer idea didn't quite work

--- Frontend/scripts/script.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function() {
-	$.get("partials/header.html", function(data) {
-		$("body").prepend(data);
-	});
-
-	$.get("partials/footer.html", function(data) {
-		$("body").append(data);
-	});
-});
