b56d142b1a44a9495310a06990e237b4e846f5ec
Thomas Owers <owerstom@gmail.com>
2015-02-21 22:55:13 +0000

    Done the rooms wizard page

--- Frontend/wizard/script.js
+++ Frontend/wizard/script.js
@@ -27,4 +27,39 @@ $(document).ready(function() {
 			$(this).find("span").addClass("wizard-content-checkbox-selected-span");
 		}
 	});
+
+	var options = {
+		url: "/file/post",
+		previewsContainer: "#wizard-rooms-uploaded",
+ 		previewTemplate: $("#wizard-rooms-uploaded").html(),
+ 		thumbnailWidth: 50,
+  		thumbnailHeight: 50,
+  		acceptedFiles: "image/*",
+  		maxFiles: 4,
+  		init: function() {
+		    this.on("addedfile", function() {
+		      if (this.files[4]!=null){
+		        this.removeFile(this.files[0]);
+		      }
+		    });
+	  	}
+	}
+
+	$("#wizard-rooms-fileupload").dropzone(options);
+	$("#wizard-rooms-fileupload span").dropzone(options);
+
+	$(document).on("click", ".wizard-room-list div", function() {
+		$(".wizard-room-list-selected").removeClass("wizard-room-list-selected");
+		$(this).addClass("wizard-room-list-selected");
+	});
+
+	$(document).on("click", "#wizard-room-meters", function() {
+		$("#wizard-room-feet").removeClass("wizard-room-selected");
+		$(this).addClass("wizard-room-selected");
+	});
+
+	$(document).on("click", "#wizard-room-feet", function() {
+		$("#wizard-room-meters").removeClass("wizard-room-selected");
+		$(this).addClass("wizard-room-selected");
+	});
 });
