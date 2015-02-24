bbe6daa9733697867be0971566b264767177d72f
jaspervandervalk <jasper.v.d.valk@gmail.com>
2015-02-24 21:02:47 +0000

    added a html version on the email and a plain text version GG EZ

--- HouseHunter/app/mailers/user_mailer.rb
+++ HouseHunter/app/mailers/user_mailer.rb
@@ -1,2 +1,9 @@
 class UserMailer < ApplicationMailer
+  default from: 'notifications@example.com'
+
+  def welcome_email(user)
+    @user = user
+    @url = 'http://homehunter.com/login'
+    mail(to: @user.email, subject: 'Welcome to HomeHunter!')
+  end
 end
