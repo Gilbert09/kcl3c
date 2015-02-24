268fce3e0331fb16584a39b5201dda1193f49d34
Tom (Server) <tom@dysign.co.uk>
2015-02-24 07:53:47 -0500

    Changed routes

--- HouseHunter/config/routes.rb
+++ HouseHunter/config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
 
   resources :property
 
+  get 'wizard' => 'wizard#index'
   get ':action' => 'static#:action'
   
   # The priority is based upon order of creation: first created -> highest priority.
