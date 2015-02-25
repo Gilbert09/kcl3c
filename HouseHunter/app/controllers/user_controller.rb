d07819d0cca1d88a8cb4446d85b01bfecf8f4119
jaspervandervalk <jasper.v.d.valk@gmail.com>
2015-02-25 18:11:29 +0000

    added a User controller to handle the email functionality.

---
+++ HouseHunter/app/controllers/user_controller.rb
@@ -0,0 +1,17 @@
+class UserController < ApplicationController
+  def create
+    @user = User.new(params[:user])
+
+    respond_to do |format|
+      if @user.save
+        UserMailer.welcome_email(@user).deliver_later
+
+        format.html { redirect_to(@user, notice: 'User was successfully created.') }
+        format.json { render json: @user, status: :created, location: @user }
+      else
+        format.html { render action: 'new' }
+        format.json { render json: @user.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+end
