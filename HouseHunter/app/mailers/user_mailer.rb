f595641b7d8cf7743a55eee2da8c3fda257c504f
jaspervandervalk <jasper.v.d.valk@gmail.com>
2015-02-27 16:13:28 +0000

    added a confirm address method

--- HouseHunter/app/mailers/user_mailer.rb
+++ HouseHunter/app/mailers/user_mailer.rb
@@ -1,6 +1,12 @@
 class UserMailer < ApplicationMailer
   default from: 'notifications@homehunter.com'
 
+  def confirm_address(user)
+    @user = user
+    @url = 'http://homehunter.com/confirm_address'
+    mail(to: @user.email, subject: 'Please confirm your email address')
+  end
+
   def welcome_email(user)
     @user = user
     @url  = 'http://homehunter.com/login'
