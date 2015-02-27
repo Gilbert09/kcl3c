35424d108a8f54ec86750f9cc135cc9f94f6dfb6
jaspervandervalk <jasper.v.d.valk@gmail.com>
2015-02-27 16:22:58 +0000

    added the method call for the confirm address but pretty sure we gonna hve to fiddle with it

--- HouseHunter/app/controllers/user_controller.rb
+++ HouseHunter/app/controllers/user_controller.rb
@@ -2,6 +2,9 @@ class UserController < ApplicationController
   def create
     @user = User.new(params[:user])
 
+    # Don't really know where to call this yet but putting it here cos it has to go out before we send the welcome email.
+    UserMailer.confirm_address(@user).deliver_now
+
     respond_to do |format|
       if @user.save
         UserMailer.welcome_email(@user).deliver_later
