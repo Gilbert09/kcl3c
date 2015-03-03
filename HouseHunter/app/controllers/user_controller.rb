afdec18e54ea7c738e8c573afe76418f155a9f29
jaspervandervalk <jasper.v.d.valk@gmail.com>
2015-03-03 15:15:17 +0000

    put the method in the wrong place

--- HouseHunter/app/controllers/user_controller.rb
+++ HouseHunter/app/controllers/user_controller.rb
@@ -5,7 +5,7 @@ class UserController < ApplicationController
     # Don't really know where to call this yet but putting it here cos it has to go out before we send the welcome email.
 
     respond_to do |format|
-      if @user.after_create
+      if @user.save
         UserMailer.confirm_address(@user).deliver_now
 
         format.html { redirect_to(@user, notice: 'User was successfully created.') }
@@ -15,17 +15,17 @@ class UserController < ApplicationController
         format.json { render json: @user.errors, status: :unprocessable_entity }
       end
     end
-  end
 
-  respond_to do |format|
-    if @user.save
-      UserMailer.welcome_email(@user).deliver_now
+    respond_to do |format|
+      if @user.after_create
+        UserMailer.welcome_email(@user).deliver_now
 
-      format.html { redirect_to(@user, notice: 'User was successfully created.') }
-      format.json { render json: @user, status: :created, location: @user }
-    else
-      format.html { render action: 'new' }
-      format.json { render json: @user.errors, status: :unprocessable_entity }
+        format.html { redirect_to(@user, notice: 'User was successfully created.') }
+        format.json { render json: @user, status: :created, location: @user }
+      else
+        format.html { render action: 'new' }
+        format.json { render json: @user.errors, status: :unprocessable_entity }
+      end
     end
   end
 end
