6823e1026cf1472ee432c4645f22c151b91cd0fa
Thomas Owers <owerstom@gmail.com>
2015-03-03 18:13:11 +0000

    Changed ApplicationController

--- HouseHunter/app/controllers/application_controller.rb
+++ HouseHunter/app/controllers/application_controller.rb
@@ -9,7 +9,7 @@ class ApplicationController < ActionController::Base
   
   def configure_permitted_parameters
     devise_parameter_sanitizer.for(:sign_up) do |u|
-		u.permit(:first_name, :last_name, :email, :password, :password_confirmation, :phone_number, :title)
+		u.permit(:email, :password, :password_confirmation)
     end
 	
 	devise_parameter_sanitizer.for(:sign_in) do |u|
