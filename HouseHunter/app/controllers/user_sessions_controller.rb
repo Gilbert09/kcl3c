26e976c110a8c429dc1be3cbb5f145250ed8f760
Thomas Owers <owerstom@gmail.com>
2015-03-03 19:25:33 +0000

    Set up controllers and views

--- HouseHunter/app/controllers/user_sessions_controller.rb
+++ HouseHunter/app/controllers/user_sessions_controller.rb
@@ -1,10 +1,21 @@
 class UserSessionsController < ApplicationController
+  skip_before_filter :require_login, except: [:destroy]
+  
   def new
+	@user = User.new
   end
 
   def create
+  	if @user = login(params[:email], param[:password])
+  		redirect_back_or_to(:users, notice: 'Login successful')
+  	else
+  		flash.now[:alert] = 'Login failed'
+  		render action 'new'
+  	end
   end
 
   def destroy
+  	logout
+  	redirect_to(:users, notice: 'Logged out!')
   end
 end
--- HouseHunter/config/routes.rb
+++ HouseHunter/config/routes.rb
@@ -1,12 +1,10 @@
 Rails.application.routes.draw do
 
-  get 'user_sessions/new'
-
-  get 'user_sessions/create'
-
-  get 'user_sessions/destroy'
-
+  resources :user_sessions
   resources :users
+
+  get 'login' => 'user_sessions#new', :as => :login
+  post 'logout' => 'user_sessions#destroy', :as => :logout
 
   root :to => 'static#index'
 
