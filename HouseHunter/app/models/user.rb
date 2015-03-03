a7b9f841cc88fc496f6fb64dc133cf7a7473914b
Tom (Server) <tom@dysign.co.uk>
2015-03-03 14:08:56 -0500

    Generated some Sorcery scaffold

--- HouseHunter/app/models/user.rb
+++ HouseHunter/app/models/user.rb
@@ -1,5 +1,2 @@
 class User < ActiveRecord::Base
-  has_many :properties, inverse_of: :user
-  authenticates_with_sorcery!
-  
 end
--- HouseHunter/config/routes.rb
+++ HouseHunter/config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
 
+  resources :users
+
   root :to => 'static#index'
 
   resources :property
