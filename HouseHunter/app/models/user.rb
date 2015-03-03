2a80f007f825063764c8a71f21c45eb20a87c9d8
Tom (Server) <tom@dysign.co.uk>
2015-03-03 13:59:38 -0500

    Init Sorcery gem

--- HouseHunter/app/models/user.rb
+++ HouseHunter/app/models/user.rb
@@ -1,8 +1,3 @@
 class User < ActiveRecord::Base
-  has_many :properties, inverse_of: :user
-	
-	# validates :first_name, presence: true
-	# validates :last_name, presence: true
-	# validates :phone_number, presence: true
-	# validates :title, presence: true
+  authenticates_with_sorcery!
 end
