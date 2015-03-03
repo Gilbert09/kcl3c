8d47b6c6029875b58b1848b0bc411f2a61af1e75
Thomas Owers <owerstom@gmail.com>
2015-03-03 19:14:22 +0000

    More Socery stuff

--- HouseHunter/app/controllers/users_controller.rb
+++ HouseHunter/app/controllers/users_controller.rb
@@ -68,7 +68,11 @@ class UsersController < ApplicationController
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
+    #def user_params
+    #  params.require(:user).permit(:email, :crypted_password, :salt)
+    #end
+
     def user_params
-      params.require(:user).permit(:email, :crypted_password, :salt)
-    end
+    params.require(:user).permit(:email, :password, :password_confirmation)
+  end
 end
