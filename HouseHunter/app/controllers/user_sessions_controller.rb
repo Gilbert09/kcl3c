9a413ade55174cb5d7a4f2f2d86e0d6c8e5460bb
Thomas Owers <owerstom@gmail.com>
2015-03-03 19:26:59 +0000

    Fixed typo

--- HouseHunter/app/controllers/user_sessions_controller.rb
+++ HouseHunter/app/controllers/user_sessions_controller.rb
@@ -1,12 +1,12 @@
 class UserSessionsController < ApplicationController
   skip_before_filter :require_login, except: [:destroy]
-  
+
   def new
 	@user = User.new
   end
 
   def create
-  	if @user = login(params[:email], param[:password])
+  	if @user = login(params[:email], params[:password])
   		redirect_back_or_to(:users, notice: 'Login successful')
   	else
   		flash.now[:alert] = 'Login failed'
