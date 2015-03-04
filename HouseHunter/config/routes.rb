338c68ca160be2ff9d262ae3422b55169e80049e
Thomas Owers <owerstom@gmail.com>
2015-03-04 16:22:36 +0000

    Sorcery pissed me off too much.. onto the next gem: Clearance

--- HouseHunter/config/routes.rb
+++ HouseHunter/config/routes.rb
@@ -1,12 +1,6 @@
 Rails.application.routes.draw do
 
-  root :to => 'users#index'
-
-  resources :user_sessions
-  resources :users
-
-  get 'login' => 'user_sessions#new', :as => :login
-  post 'logout' => 'user_sessions#destroy', :as => :logout
+  root :to => 'static#index'
 
   resources :property
 
