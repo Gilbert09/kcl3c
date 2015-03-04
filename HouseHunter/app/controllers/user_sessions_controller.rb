b3688de0a0889a174230dc1dac10bf5bb87b35a9
Thomas Owers <owerstom@gmail.com>
2015-03-04 15:53:04 +0000

    More changes to authentication stuff

--- HouseHunter/app/controllers/user_sessions_controller.rb
+++ HouseHunter/app/controllers/user_sessions_controller.rb
@@ -1,21 +1,25 @@
 class UserSessionsController < ApplicationController
   skip_before_filter :require_login, :except => [:destroy]
-
+  
   def new
     @user = User.new
   end
-
+  
   def create
-    if @user = login(params[:email], params[:password])
-      redirect_back_or_to(:users, :notice => 'Login successfull.')
-    else
-      flash.now[:alert] = 'Login failed'
-      render action: 'new'
+    respond_to do |format|
+      if @user = login(params[:email],params[:password],params[:remember])
+        format.html { redirect_back_or_to(:users, :notice => 'Login successfull.') }
+        format.xml { render :xml => @user, :status => :created, :location => @user }
+      else
+        format.html { flash.now[:alert] = "Login failed."; render :action => "new" }
+        format.xml { render :xml => @user.errors, :status => :unprocessable_entity }
+      end
     end
   end
-
+  
   def destroy
     logout
     redirect_to(:users, :notice => 'Logged out!')
   end
+
 end
