28795ed0ccb66a27e9cd38a7f2ae3492cab581cb
Tom <thomas.owers@kcl.ac.uk>
2015-03-06 16:22:46 +0000

    More config stuff

--- HouseHunter/config/initializers/clearance.rb
+++ HouseHunter/config/initializers/clearance.rb
@@ -1,9 +1,5 @@
 Clearance.configure do |config|
   config.allow_sign_up = true
-  config.cookie_domain = 'linux.dysign.co.uk'
-  config.cookie_expiration = lambda { |cookies| 1.year.from_now.utc }
-  config.cookie_name = 'remember_token'
-  config.cookie_path = '/'
   config.routes = false
   config.httponly = false
   config.mailer_sender = 'reply@example.com'
