200408a41f27cf941a98c92630ea953f0b465993
jaspervandervalk <jasper.v.d.valk@gmail.com>
2015-03-09 15:04:03 +0000

    added methods for the confirmation and welcome email in users controller and added URLs to user mailer class

--- HouseHunter/app/controllers/users_controller.rb
+++ HouseHunter/app/controllers/users_controller.rb
@@ -1,10 +1,16 @@
 class UsersController < ApplicationController
+  def new
+    @user = User.new(params[:users])
+
+    UserMailer.confirm_address(@user).deliver_now
+  end
+
   def create
     @user = User.new(params[:users])
 
     respond_to do |format|
       if @user.save
-        UserMailer.confirm_address(@user).deliver_now
+        UserMailer.welcome_email(@user).deliver_now
 
         format.html { redirect_to(@user, notice: 'User was sent the confirmation email.') }
         format.json { render json: @user, status: :created, location: @user }
--- HouseHunter/app/mailers/user_mailer.rb
+++ HouseHunter/app/mailers/user_mailer.rb
@@ -3,7 +3,7 @@ class UserMailer < ApplicationMailer
 
   def confirm_address(user)
     @user = user
-    @url = 'http://homehunter.com/registrationConfirmation'
+    @url = 'http://homehunter.com/confirmation'
     mail(to: @user.email, subject: 'Please confirm your email address')
   end
 
