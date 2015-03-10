ffa463ca9f9578cbf892b90e4a595e65490a4715
Tom <thomas.owers@kcl.ac.uk>
2015-03-10 13:38:23 +0000

    Some routes stuff

--- HouseHunter/config/routes.rb
+++ HouseHunter/config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
 
   
   resources :passwords, controller: 'clearance/passwords', only: [:create, :new]
-  resource :session, controller: 'clearance/sessions', only: [:create]
+  resource :session, controller: 'clearance/sessions', only: [:create, :destroy]
 
   resources :users, controller: 'users', only: [:create] do
     resource :password,
