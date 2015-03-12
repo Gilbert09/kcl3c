fa34d0f4415af69c7ca6c8fd30e75e9fb081bbf2
Tom <thomas.owers@kcl.ac.uk>
2015-03-12 15:17:09 +0000

    Api controller stuff

--- HouseHunter/app/controllers/api_controller.rb
+++ HouseHunter/app/controllers/api_controller.rb
@@ -34,7 +34,18 @@ class ApiController < ApplicationController
       current_user.save
       return JSON.parse('{ "result": "Success", "message": "Data saved" }');
     when "2"
+      property = Property.new
+      address = Address.new
+      property.address_id = address.id;
+      property.save
 
+      address.house_name_number = jsonData["house_name_number"]
+      address.address_line_1 = jsonData["address_line_1"]
+      address.address_line_2 = jsonData["address_line_2"]
+      address.town_city = jsonData["city"]
+      address.county = jsonData["county"]
+      address.post_code = jsonData["postcode"]
+      address.save
     when "3"
 
     when "4"
