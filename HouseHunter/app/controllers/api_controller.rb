210dc4bd81f4ea527d42a06b9e2a109b452fc4ad
Tom <thomas.owers@kcl.ac.uk>
2015-03-12 13:47:20 +0000

    Updated wizard controller

--- HouseHunter/app/controllers/api_controller.rb
+++ HouseHunter/app/controllers/api_controller.rb
@@ -26,7 +26,7 @@ class ApiController < ApplicationController
 
   def saveDraft(jsonData, stage)
     case stage
-    when 1
+    when "1"
       user = User.find(jsonData.id)
       user.first_name = jsonData.first_name;
       user.last_name = jsonData.last_name;
@@ -34,24 +34,24 @@ class ApiController < ApplicationController
       user.phone_number = jsonData.phone_number;
       user.save
       return user.id
-    when 2
+    when "2"
 
-    when 3
+    when "3"
 
-    when 4
+    when "4"
 
-    when 5
+    when "5"
 
-    when 6
+    when "6"
 
-    when 7
+    when "7"
 
-    when 8
+    when "8"
 
-    when 9
+    when "9"
 
     else 
-      return "else"
+      return stage
     end
 
     return true
