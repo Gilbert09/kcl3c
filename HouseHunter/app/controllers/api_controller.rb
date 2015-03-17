6435c1377383323b9e168c4f52baa6a544414138
Thomas Owers <owerstom@gmail.com>
2015-03-17 14:25:03 +0000

    Added better indentation

--- HouseHunter/app/controllers/api_controller.rb
+++ HouseHunter/app/controllers/api_controller.rb
@@ -26,83 +26,87 @@ class ApiController < ApplicationController
 
   def saveDraft(jsonData, stage, current_user)
     case stage
-    when "1"
-      current_user.first_name = jsonData["first_name"]
-      current_user.last_name = jsonData["last_name"]
-      current_user.email = jsonData["email"]
-      current_user.phone_number = jsonData["phone_number"]
-      current_user.save
-      return JSON.parse('{ "result": "Success", "message": "Data saved" }');
-    when "2"
-      newAddress = true
-      if jsonData["address_id"] != nil
-        current_user.properties.each do |p|
-          p.addresses.each do |a|
-            if a.id == jsonData["address_id"].to_s then newAddress = false;
+      when "1"
+        current_user.first_name = jsonData["first_name"]
+        current_user.last_name = jsonData["last_name"]
+        current_user.email = jsonData["email"]
+        current_user.phone_number = jsonData["phone_number"]
+        current_user.save
+        return JSON.parse('{ "result": "Success", "message": "Data saved" }');
+      when "2"
+        newAddress = true
+        if jsonData["address_id"] != nil
+          current_user.properties.each do |p|
+            p.addresses.each do |a|
+              if a.id == jsonData["address_id"].to_s then newAddress = false;
+            end
           end
         end
-      end
 
-      if newAddress then
-        property = Property.new
-        address = Address.new
+        if newAddress == true and jsonData["address_id"] != nil then
+          return JSON.parse('{ "result": "Error", "message": "Invalid address ID" }');
+        end
+
+        if newAddress then
+          property = Property.new
+          address = Address.new
+          property.save
+          address.save
+
+          address.property_id = proeprty.id
+          property.user_id = current_user.id
+          property.address_id = address.id
+          property.save
+
+          address.house_name_number = jsonData["house_name_number"]
+          address.address_line_1 = jsonData["address_line_1"]
+          address.address_line_2 = jsonData["address_line_2"]
+          address.town_city = jsonData["city"]
+          address.county = jsonData["county"]
+          address.post_code = jsonData["postcode"]
+          address.save
+        else
+          
+        end
+      when "3"
+        property.property_type = jsonData["property_date"]
+        property.entrance_floor = jsonData["entrance_floor"]
+        property.number_of_bedrooms = jsonData["number_of_bedrooms"]
+        for item in property.number_of_bedrooms
+          bedroom = Room.new
+        end
+        property.number_of_receptions = jsonData["number_of_receptions"]
+        for item in property.number_of_receptions
+          reception = Room.new
+        end
+        property.number_of_floors = jsonData["number_of_floors"]
+        property.condition = jsonData["condition"]
+        property.number_of_bathrooms = jsonData["number_of_bathrooms"]
+        for item in property.number_of_bathrooms
+          bathroom = Room.new
+        end
+        property.number_of_other_rooms = jsonData["number_of_other_rooms"]
+        for item in property.number_of_other_rooms
+          otherRoom = Room.new
+        end
         property.save
-        address.save
-
-        address.property_id = proeprty.id
-        property.user_id = current_user.id
-        property.address_id = address.id
+      when "4"
+        property.outdoor_space = jsonData["outdoor_space"]
         property.save
+      when "5"
+        property.parking = jsonData["parking"]
+        property.save
+      when "6"
+        property.heating = jsonData["heating"]
+        property.save
+      when "7"
 
-        address.house_name_number = jsonData["house_name_number"]
-        address.address_line_1 = jsonData["address_line_1"]
-        address.address_line_2 = jsonData["address_line_2"]
-        address.town_city = jsonData["city"]
-        address.county = jsonData["county"]
-        address.post_code = jsonData["postcode"]
-        address.save
-      else
-        
-      end
-    when "3"
-      property.property_type = jsonData["property_date"]
-      property.entrance_floor = jsonData["entrance_floor"]
-      property.number_of_bedrooms = jsonData["number_of_bedrooms"]
-      for item in property.number_of_bedrooms
-        bedroom = Room.new
-      end
-      property.number_of_receptions = jsonData["number_of_receptions"]
-      for item in property.number_of_receptions
-        reception = Room.new
-      end
-      property.number_of_floors = jsonData["number_of_floors"]
-      property.condition = jsonData["condition"]
-      property.number_of_bathrooms = jsonData["number_of_bathrooms"]
-      for item in property.number_of_bathrooms
-        bathroom = Room.new
-      end
-      property.number_of_other_rooms = jsonData["number_of_other_rooms"]
-      for item in property.number_of_other_rooms
-        otherRoom = Room.new
-      end
-      property.save
-    when "4"
-      property.outdoor_space = jsonData["outdoor_space"]
-      property.save
-    when "5"
-      property.parking = jsonData["parking"]
-      property.save
-    when "6"
-      property.heating = jsonData["heating"]
-      property.save
-    when "7"
+      when "8"
 
-    when "8"
+      when "9"
 
-    when "9"
-
-    else 
-      return JSON.parse('{ "result": "Error", "message": "Invalid stage" }');
+      else 
+        return JSON.parse('{ "result": "Error", "message": "Invalid stage" }');
     end
   end
 end
