b2dcfae88a72f5fbd4bebe4cff1189178391ac34
Thomas Owers <owerstom@gmail.com>
2015-03-17 15:25:25 +0000

    Onto the next stage!

--- HouseHunter/app/controllers/api_controller.rb
+++ HouseHunter/app/controllers/api_controller.rb
@@ -89,8 +89,7 @@ class ApiController < ApplicationController
         address.county = jsonData["county"]
         address.post_code = jsonData["postcode"]
         address.save
-        json = '{ "result": "Success", "message": "Data saved", "data": { "address_id": ' + address.id.to_s +', "property_id": ' + property.id.to_s + ' } }'
-        return JSON.parse(json)
+        return JSON.parse("{ \"result\": \"Success\", \"message\": \"Data saved\", \"data\": { \"address_id\": " + address.id.to_s +", \"property_id\": " + property.id.to_s + " } }")
       else
         address = Address.find(jsonData["address_id"] )
         address.house_name_number = jsonData["house_name_number"]
