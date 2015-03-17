a7c68f91bb5007333593c250ca6f4c1eac215c28
Thomas Owers <owerstom@gmail.com>
2015-03-17 14:30:11 +0000

    indentation fix

--- HouseHunter/app/controllers/api_controller.rb
+++ HouseHunter/app/controllers/api_controller.rb
@@ -38,8 +38,7 @@ class ApiController < ApplicationController
         if jsonData["address_id"] != nil
           current_user.properties.each do |p|
             p.addresses.each do |a|
-              if a.id == jsonData["address_id"].to_s then newAddress = false;
-              end
+              if a.id == jsonData["address_id"].to_s then newAddress = false end
             end
           end
         end
