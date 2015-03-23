b3e4ea4450bfa15f42828168bb1375f2bdce620a
jaspervandervalk <jasper.v.d.valk@gmail.com>
2015-03-23 09:58:53 +0000

    commented the rightmove controller page

--- HouseHunter/app/controllers/rightmove_controller.rb
+++ HouseHunter/app/controllers/rightmove_controller.rb
@@ -1,6 +1,12 @@
 class RightmoveController < ApplicationController
+
+=begin
+this class is the controller for the Rightmove API.
+=end
+
   @data = Hash.new
 
+  # this method initializes the class and takes in the data of a property and converts it to a json data format.
   def initialize(propertyData)
     @data.to_json
   end
