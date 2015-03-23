259fc60b6fac8584abf325f566db3e2dcebf0ebb
jaspervandervalk <jasper.v.d.valk@gmail.com>
2015-03-23 10:23:29 +0000

    commented the users controller page

--- HouseHunter/app/controllers/users_controller.rb
+++ HouseHunter/app/controllers/users_controller.rb
@@ -1,8 +1,14 @@
 class UsersController < Clearance::UsersController
+
+=begin
+this class represents the controller for all users, users must be signed in for the functionality of this class.
+=end
+
   before_filter :redirect_signed_in_users, only: [:create, :new]
   skip_before_filter :require_login, only: [:create, :new]
   skip_before_filter :authorize, only: [:create, :new]
 
+  # this method is called when a user creates a new user account on the website, it sends an email to the user asking them to confirm their account creation.
   def new
     @user = user_from_params
     render template: "users/new"
@@ -10,6 +16,9 @@ class UsersController < Clearance::UsersController
     UserMailer.confirm_address(@user).deliver_now
   end
 
+  # this method is called when a user has confirmed the creation of their account by clicking on the link in the email,
+  # it then saves the users details in the database and sends them an email letting them know they have finished their
+  # account creation.
   def create
     @user = user_from_params
 
@@ -22,8 +31,11 @@ class UsersController < Clearance::UsersController
     end
   end
 
+
+  # all the methods below this line are private.
   private
 
+  # this method is called when a user signs in.
   def avoid_sign_in
     warn "[DEPRECATION] Clearance's `avoid_sign_in` before_filter is " +
       "deprecated. Use `redirect_signed_in_users` instead. " +
@@ -32,6 +44,7 @@ class UsersController < Clearance::UsersController
     redirect_signed_in_users
   end
 
+  # this method is called when a user has signed in, it redirects them to the non static version of the web pages.
   def redirect_signed_in_users
     if signed_in?
       redirect_to Clearance.configuration.redirect_url
@@ -42,6 +55,7 @@ class UsersController < Clearance::UsersController
     Clearance.configuration.redirect_url
   end
 
+  # this method deletes a user from the user model.
   def user_from_params
     email = user_params.delete(:email)
     password = user_params.delete(:password)
@@ -52,6 +66,7 @@ class UsersController < Clearance::UsersController
     end
   end
 
+  # this method creates a hash of all the currently signed in users.
   def user_params
     params[:user] || Hash.new
   end
