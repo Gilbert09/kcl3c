614525106d940eed324eadf178e3d96d1e17700a
jaspervandervalk <jasper.v.d.valk@gmail.com>
2015-03-23 10:37:13 +0000

    commented the listing model class

--- HouseHunter/app/models/listing.rb
+++ HouseHunter/app/models/listing.rb
@@ -1,3 +1,9 @@
 class Listing < ActiveRecord::Base
+
+=begin
+this class defines the model for the listings in the database. It belongs to a property and again is reversible so
+a user can find a listing through a property.
+=end
+
   belongs_to :property, inverse_of: :listing
 end
