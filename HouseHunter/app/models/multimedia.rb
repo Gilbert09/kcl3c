d405ff005cb3c1a89e6254964ba2776bf19e0da5
jaspervandervalk <jasper.v.d.valk@gmail.com>
2015-03-23 10:39:42 +0000

    commented the multimedia model class

--- HouseHunter/app/models/multimedia.rb
+++ HouseHunter/app/models/multimedia.rb
@@ -1,4 +1,10 @@
 class Multimedia < ActiveRecord::Base
+
+=begin
+this class defines the model for multimedia in the database. Multimedia belongs to property and also a room this allowed
+us to add room specific media or property specific media.
+=end
+
   belongs_to :property, inverse_of: :multimedias
   belongs_to :room, inverse_of: :multimedias
 end
