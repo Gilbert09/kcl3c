34a1e141e197324ff595ed120d9e7625aae847b4
jaspervandervalk <jasper.v.d.valk@gmail.com>
2015-03-23 10:48:37 +0000

    commented the room model class

--- HouseHunter/app/models/room.rb
+++ HouseHunter/app/models/room.rb
@@ -1,4 +1,9 @@
 class Room < ActiveRecord::Base
+
+=begin
+this class defines the model for room in the database. A room belongs to a property and can have many instances of multimedia.
+=end
+
   belongs_to :property, inverse_of: :rooms
   has_many :multimedias
 end
