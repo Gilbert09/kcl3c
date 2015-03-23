f070e88864c995256de9c7f0d5bd74f499f55e70
jaspervandervalk <jasper.v.d.valk@gmail.com>
2015-03-23 10:24:49 +0000

    commented the  application mailer page

--- HouseHunter/app/mailers/application_mailer.rb
+++ HouseHunter/app/mailers/application_mailer.rb
@@ -1,4 +1,9 @@
 class ApplicationMailer < ActionMailer::Base
+
+=begin
+this is the mailer class it defines the default address emails are sent from and their default layout.
+=end
+
   default "no-reply@homehunter.com"
   layout 'mailer'
 end
