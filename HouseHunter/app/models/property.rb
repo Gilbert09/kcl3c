7e7a051741119a5df12ef23c1d0bc140e4d14ba0
jaspervandervalk <jasper.v.d.valk@gmail.com>
2015-03-23 10:43:35 +0000

    commented the property model class

--- HouseHunter/app/models/property.rb
+++ HouseHunter/app/models/property.rb
@@ -1,4 +1,10 @@
 class Property < ActiveRecord::Base
+
+=begin
+this class defines the model for properties in the database. Every instance of a property belongs to a user, has ownership
+over many instances of multimedia, has many instances of rooms and has one listing and address.
+=end
+
   belongs_to :user, inverse_of: :properties
   has_many :multimedias
   has_many :rooms
