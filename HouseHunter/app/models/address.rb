484cdc5ea884915b3e543bfcd4217168baeb30d7
jaspervandervalk <jasper.v.d.valk@gmail.com>
2015-03-23 10:33:02 +0000

    commented the address model class

--- HouseHunter/app/models/address.rb
+++ HouseHunter/app/models/address.rb
@@ -1,3 +1,9 @@
 class Address < ActiveRecord::Base
+
+=begin
+this class defines the model for addresses in the database, it creates the relations that define the address
+the address field belongs has a property and is reversible meaning you can find a property by looking for an address.
+=end
+
   belongs_to :property, inverse_of: :address
 end
