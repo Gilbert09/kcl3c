b0c01ec8ac53987c57242b28edb4c961d6fb0687
jaspervandervalk <jasper.v.d.valk@gmail.com>
2015-03-23 09:54:03 +0000

    commented the property controller page

--- HouseHunter/app/controllers/property_controller.rb
+++ HouseHunter/app/controllers/property_controller.rb
@@ -1,19 +1,29 @@
 class PropertyController < ApplicationController
 
+=begin
+this class is a controller for any properties which are uploaded to the website, this class requires authorization in
+order to use as we need to ensure that a user can only manage their own properties.
+=end
+
   before_filter :authorize
 
+
+  # this method is called when a user wants to view all of their properties, it makes a call to the database to find the users properties.
   def show
     @property = Property.find(params[:id])
   end
 
+  # this method shows the user all of their properties.
   def index
     @properties = Property.all
   end
 
+  # this method is called when a user wants to add a new property.
   def new
     @property = Property.new
   end
 
+  # this method is called when a user has created a new property, it saves the property in the database.
   def create
     @property = Property.new
     if @property.save
@@ -23,10 +33,12 @@ class PropertyController < ApplicationController
     end
   end
 
+  # this method is called when a user wants to edit a property.
   def edit
     @property = Property.find(params[:id])
   end
 
+  # this method is called when a user has finished editing a property, it saves the updated details in the database.
   def update
     @property = Property.find(params[:id])
     if @property.update_attributes(property_params)
@@ -36,16 +48,18 @@ class PropertyController < ApplicationController
     end
   end
 
-  # these methods (delete && destroy) below may be deleted depending on client requests.
+  # this method is called when a user makes a request to delete a property, it fetches the property from the database.
   def delete
     @property = Property.find(params[:id])
   end
 
+  # this method is called when a user deletes a property, it removes the property from the database.
   def destroy
     @property = Property.find(params[:id]).destroy
     redirect_to(:action => 'index')
   end
 
+  # this method identifies what is required in order to access any of the methods above.
   private
   def property_params
     params.require(:property).permit(:price, :numberBedroom, :numberBathroom, :numberRoom, :keyword, :description, :type)
