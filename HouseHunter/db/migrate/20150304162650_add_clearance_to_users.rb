d37338ec76701ebbd881db2df8d5daa584a08967
jaspervandervalk <jasper.v.d.valk@gmail.com>
2015-03-23 11:11:34 +0000

    commented the clearence to users fields.

--- HouseHunter/db/migrate/20150304162650_add_clearance_to_users.rb
+++ HouseHunter/db/migrate/20150304162650_add_clearance_to_users.rb
@@ -1,4 +1,11 @@
 class AddClearanceToUsers < ActiveRecord::Migration
+
+=begin
+this class is the database migration for clearance for users, this class provides users with tokens used to define whether
+a user is online and encrypt their password in the database.
+=end
+
+  # this method creates fields in the database which allow for new fields.
   def self.up
     change_table :users  do |t|
       t.string :email
@@ -20,6 +27,7 @@ class AddClearanceToUsers < ActiveRecord::Migration
     end
   end
 
+  # this method edits the users table in the database by encrypting their password and adding a confirmation token.
   def self.down
     change_table :users do |t|
       t.remove :encrypted_password,:confirmation_token,:remember_token
