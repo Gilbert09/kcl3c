c8d7d5d757eb9e8a407ba19c122ec41406ad443c
jaspervandervalk <jasper.v.d.valk@gmail.com>
2015-03-23 10:48:58 +0000

    commented the user model class

--- HouseHunter/app/models/user.rb
+++ HouseHunter/app/models/user.rb
@@ -1,4 +1,11 @@
 class User < ActiveRecord::Base
+
+=begin
+this class represents the model for users in the database. The user includes clearance meaning they have to be registered
+before their details are stored in the database. A user can have many instances of properties. And a user can be searched
+to find all of their properties.
+=end
+
 	include Clearance::User
 
 	has_many :properties, inverse_of: :user
